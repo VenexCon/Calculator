7cccf1b585f0d7bdfbfd0ae025549725895f7a2b
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-17 23:00:59 +0100

    Updated JS, collated all eventlistsners in forEach

--- script.js
+++ script.js
@@ -37,76 +37,16 @@ function operate (a,b,operand) {
 }
 
 
-//grid Calc Buttons
-const allBtns = document.querySelectorAll(`.calcbuttons`).forEach(Element => Element.addEventListener(`click`, (e) =>{
-    console.log(e.target.innertext);
-}) )
+//number buttons 
 
-addEventListener(`click`, (e) => {
-    console.log(e.target.innertext);
-})
+const numberBtns = document.querySelectorAll(`.numberButton`)
+    numberBtns.forEach(btn => btn.addEventListener(`click`, (e) =>{
+    console.log(parseInt(e.target.innerText));
+    }));
 
-
-const addBtn = document.getElementById(`add`).addEventListener(`click`, (e) => {
-        console.log(e.target);
-    })
-
-const subtractBtn = document.getElementById(`minus`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const divideBtn = document.getElementById(`divide`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const timesBtn = document.getElementById(`times`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const sevenBtn = document.getElementById(`seven`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const eightBtn = document.getElementById(`eight`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const nineBtn = document.getElementById(`nine`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const zeroBtn = document.getElementById(`zero`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const fourBtn = document.getElementById(`four`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const fiveBtn = document.getElementById(`five`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const sixBtn = document.getElementById(`six`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const EqualsBtn = document.getElementById(`equals`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const oneBtn = document.getElementById(`one`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const twoBtn = document.getElementById(`two`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const threeBtn = document.getElementById(`three`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
-
-const allClearBtn = document.getElementById(`allClear`).addEventListener(`click`, (e) => {
-    console.log(e.target);
-})
+    
+//Operand buttons
+const operatorBtns = document.querySelectorAll(`.operatorButton`)
+    operatorBtns.forEach(btn => btn.addEventListener(`click`, (e) =>{
+    console.log("e.target.innerText");
+    }));
