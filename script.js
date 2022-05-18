aa0a2de9d3b3b1110c207b94ece462ce77056cc6
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-18 18:10:28 +0100

    <update> to JS to log inner.text

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-
 // create basic functions - Done 
 //create a function that an operand can be passed to - completed using Switch statements
 //store numbers as a held variable, once an operand is selected the number should be pushed to an array. 
 //the operand should be pushed to another variable, or a variable called. 
 // repeat until equals sign is pressed. 
 
+//CONSTS
+const displayText = document.getElementById("display") 
 
 
 
 
 
 // Base level Functions 
-function add(a, b) {
-    return (a + b);
-}
+const add = (a,b) => (a+b);
 const minus = (a, b) => (a-b); 
 const divide = (a,b) => (a/b); 
 const times = (a, b) => (a * b)
@@ -37,16 +36,19 @@ function operate (a,b,operand) {
 }
 
 
-//number buttons 
 
 const numberBtns = document.querySelectorAll(`.numberButton`)
     numberBtns.forEach(btn => btn.addEventListener(`click`, (e) =>{
     console.log(parseInt(e.target.innerText));
     }));
 
-    
-//Operand buttons
 const operatorBtns = document.querySelectorAll(`.operatorButton`)
     operatorBtns.forEach(btn => btn.addEventListener(`click`, (e) =>{
-    console.log("e.target.innerText");
+    console.log(e.target.innerText);
     }));
+ 
+function updateDisplay (arg) {
+  return displayText.innerText = arg;
+}
+
+console.log(updateDisplay("+"))
